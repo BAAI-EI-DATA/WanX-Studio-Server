be7298f020d7ed86157ee8f69eb37c85d1b9ee6b
liuyou1103 <2256309953@qq.com>
2025-10-16 16:37:30 +0800

    update for path

--- x86/start_server_docker.sh
+++ x86/start_server_docker.sh
@@ -14,7 +14,7 @@ CURRENT_USER=$(whoami)
 
 # 动态构建卷挂载路径
 VOLUMES="-v /home/${CURRENT_USER}/DoRobot/dataset/:/home/robot/dataset/"
-VOLUMES2="-v /home/${CURRENT_USER}/WanX-Studio-Server/x86/:/app/code/"
+VOLUMES2="-v /opt/WanX-Studio-Server/x86/:/app/code/"
 VOLUMES3="-v /opt/wanx_studio/:/home/machine/"
 
 # ====================== 逻辑部分（增强版） ======================
